769d5bb40df86d09f74339f69e60a27362401dbb
BBTBB <abassbah43@gmail.com>
2021-07-18 02:11:59 +0300

    Update add_bot.lua

--- JokerTeamFiles/add_bot.lua
+++ JokerTeamFiles/add_bot.lua
@@ -1,18 +1,18 @@
 --[[
-BY : TshAkETEAM
-Channel Files : https://t.me/tshakeFiles
+BY : JokerTeamTEAM
+Channel Files : https://t.me/JokerTeamFiles
 ]]
-local function keko_tshake(data)
+local function keko_JokerTeam(data)
 local msg = data.message_
-redis = (loadfile "./libs/redis.lua")()
+redis = (loadfile "./redis.lua")()
 database = Redis.connect('127.0.0.1', 6379)
 sudos = dofile('sudo.lua')
 HTTPS = require("ssl.https")
-JSON = (loadfile  "./libs/dkjson.lua")()
+JSON = (loadfile  "./dkjson.lua")()
 bot_id_keko = {string.match(token, "^(%d+)(:)(.*)")}
 bot_id = tonumber(bot_id_keko[1])
 msg = data.message_
-bot=dofile('./libs/utils.lua')
+bot=dofile('./Joker.lua')
 text = msg.content_.text_
 local function send(chat_id, reply_to_message_id, disable_notification, text, disable_web_page_preview, parse_mode)
 local TextParseMode = {ID = "TextParseModeMarkdown"}
@@ -45,49 +45,49 @@ local var = false
 for k,v in pairs(sudo_users) do
 if msg.sender_user_id_ == v then var = true end
 end
-local keko_add_sudo = redis:get('tshake:'..bot_id..'sudoo'..msg.sender_user_id_..'')
+local keko_add_sudo = redis:get('JokerTeam:'..bot_id..'sudoo'..msg.sender_user_id_..'')
 if keko_add_sudo then var = true end return var
 end
-if (text and text == "تفعيل" and is_sudo(msg) and database:get("tshake:get_back_add_bot:"..bot_id)) then 
-if database:get("tshake:link:auto:launch"..bot_id) then 
-database:set("lock_link:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_inline:tshake"..msg.chat_id_..bot_id,"ok")
+if (text and text == "تفعيل" and is_sudo(msg) and database:get("JokerTeam:get_back_add_bot:"..bot_id)) then 
+if database:get("JokerTeam:link:auto:launch"..bot_id) then 
+database:set("lock_link:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_inline:JokerTeam"..msg.chat_id_..bot_id,"ok")
 end 
-if database:get("tshake:fwd:auto:launch"..bot_id) then 
-database:set("lock_fwd:tshake"..msg.chat_id_..bot_id,"ok")
+if database:get("JokerTeam:fwd:auto:launch"..bot_id) then 
+database:set("lock_fwd:JokerTeam"..msg.chat_id_..bot_id,"ok")
 end 
-if database:get("tshake:medea:auto:launch"..bot_id) then 
-database:set("lock_media:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_audeo:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_video:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_photo:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_stecker:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_voice:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_gif:tshake"..msg.chat_id_..bot_id,"ok")
-database:set("lock_note:tshake"..msg.chat_id_..bot_id,"ok")
+if database:get("JokerTeam:medea:auto:launch"..bot_id) then 
+database:set("lock_media:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_audeo:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_video:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_photo:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_stecker:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_voice:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_gif:JokerTeam"..msg.chat_id_..bot_id,"ok")
+database:set("lock_note:JokerTeam"..msg.chat_id_..bot_id,"ok")
 end 
-if database:get("tshake:username:auto:launch"..bot_id) then 
-database:set("lock_username:tshake"..msg.chat_id_..bot_id,"ok")
+if database:get("JokerTeam:username:auto:launch"..bot_id) then 
+database:set("lock_username:JokerTeam"..msg.chat_id_..bot_id,"ok")
 end 
-if database:get("tshake:spam:auto:launch"..bot_id) then 
-database:set("lock_lllll:tshake"..msg.chat_id_..bot_id,"ok")
+if database:get("JokerTeam:spam:auto:launch"..bot_id) then 
+database:set("lock_lllll:JokerTeam"..msg.chat_id_..bot_id,"ok")
 end 
 
-if database:get("tshake:auto:seva:admin:auto:launch"..bot_id) then 
+if database:get("JokerTeam:auto:seva:admin:auto:launch"..bot_id) then 
 local function cb2(extra,result,success)
 for k,v in pairs(result.members_) do
-database:sadd('tshake:'..bot_id..'mods:'..msg.chat_id_, v.user_id_)
+database:sadd('JokerTeam:'..bot_id..'mods:'..msg.chat_id_, v.user_id_)
 end
 end
 bot.channel_get_admins(msg.chat_id_,cb2)
 end
 
-if database:get("tshake:spam:auto:launch"..bot_id) then 
+if database:get("JokerTeam:spam:auto:launch"..bot_id) then 
 
 local function cb_ts(t1,t2)
 for k,v in pairs(t2.members_) do
 if v.status_.ID == "ChatMemberStatusCreator" then 
-database:sadd('tshake:'..bot_id..'creator:'..msg.chat_id_, v.user_id_)
+database:sadd('JokerTeam:'..bot_id..'creator:'..msg.chat_id_, v.user_id_)
 end
 end
 end
@@ -136,10 +136,10 @@ a = "auto:save:crete"
 end
 
 if u then 
-database:set("tshake:"..u..":auto:launch"..bot_id,"ok")
+database:set("JokerTeam:"..u..":auto:launch"..bot_id,"ok")
 send(msg.chat_id_, msg.id_, 1, "✔┇تم الحفظ ", 1, 'html')
 elseif a then 
-database:del("tshake:"..a..":auto:launch"..bot_id)
+database:del("JokerTeam:"..a..":auto:launch"..bot_id)
 send(msg.chat_id_, msg.id_, 1, "✔┇تم الحفظ ", 1, 'html')
 end
 
@@ -148,19 +148,19 @@ end
 
 if (text and text == "تفعيل الاعدادات المسبقه" and tonumber(msg.sender_user_id_) == tonumber(sudo_add)) then 
 send(msg.chat_id_, msg.id_, 1, "✔┇تم تفعيل عمليه الاعدادات المسبقه عند التفعيل ", 1, 'html')
-database:set("tshake:get_back_add_bot:"..bot_id,"ok")
+database:set("JokerTeam:get_back_add_bot:"..bot_id,"ok")
 end 
 
 if (text and text == "تعطيل الاعدادات المسبقه" and tonumber(msg.sender_user_id_) == tonumber(sudo_add)) then 
 send(msg.chat_id_, msg.id_, 1, "🔓┇تم تعطيل خاصيه الاعدادات المسبقه عند التفعيل", 1, 'html')
-database:del("tshake:get_back_add_bot:"..bot_id)
+database:del("JokerTeam:get_back_add_bot:"..bot_id)
 end 
 
 end
 return {
-keko_tshake = keko_tshake,
+keko_JokerTeam = keko_JokerTeam,
 }
 --[[
-BY : TshAkETEAM
-Channel Files : https://t.me/tshakeFiles
+BY : JokerTeamTEAM
+Channel Files : https://t.me/JokerTeamFiles
 ]]
